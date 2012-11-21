9ef0c3c74295e7b03fb362030cb67e6f7e4684c3
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-11-21 16:36:25 +0100

    Remove support for Lenny    - End of Life on 2012-02-06

--- manifests/whitelist.pp
+++ manifests/whitelist.pp
@@ -4,32 +4,13 @@ define icinga::whitelist($whitelist = false) {
     default => $whitelist,
   }
 
-  if $::lsbdistcodename == 'lenny' {
-    file { $name:
-      owner   => root,
-      group   => root,
-      mode    => '0644',
-      alias   => 'nrpe.cfg',
-      notify  => Service['nagios-nrpe-server'],
-      content => template("icinga/${::lsbdistcodename}/etc/nagios/nrpe.cfg.erb"),
-      require => Package['nagios-nrpe-server'],
-    }
-
-    file { '/var/run/nagios':
-      ensure => directory,
-      owner  => nagios,
-      group  => root,
-      mode   => '0755',
-    }
-  } else {
-    file { $name:
-      owner   => root,
-      group   => root,
-      mode    => '0644',
-      alias   => 'nrpe.cfg',
-      notify  => Service['nagios-nrpe-server'],
-      content => template('icinga/common/etc/nagios/nrpe.cfg.erb'),
-      require => Package['nagios-nrpe-server'],
-    }
+  file { $name:
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    alias   => 'nrpe.cfg',
+    notify  => Service['nagios-nrpe-server'],
+    content => template('icinga/common/etc/nagios/nrpe.cfg.erb'),
+    require => Package['nagios-nrpe-server'],
   }
 }
